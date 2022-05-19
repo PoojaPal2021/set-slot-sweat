1f98379ec42d254cac262f78eabb97a3da6aec29
Pooja Pal <poojapal@uw.edu>
2022-05-19 10:41:24 -0700

    API Integration in Progress :: Code version has CORS Errors

--- User-Interface/src/app/services/schedule-session.service.ts
+++ User-Interface/src/app/services/schedule-session.service.ts
@@ -1,80 +1,138 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { schedule } from '../models/schedule';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { apiendpoints } from '../../app/api-endpoints'
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class ScheduleSessionService {
   configUrl = 'assets/config.json';
   constructor(private http: HttpClient) { }
 
-  loadProfileData() {
-    return this.http.get<schedule>(this.configUrl);
-  }
-  authenticateUser(loginForm: FormGroup) {
-    return this.http.get<boolean>('');
-  }
+  registerNewMember(profile: FormGroup) 
+  {
+    console.log("INSIDE SERVICE ::registerNewMember ")
 
-  registerUser(profile: FormGroup) {
-    console.log("Inside register user with form")
-
-    fetch(apiendpoints.REGISTER_USER_URL, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(profile),
-    })
-      .then((response) => response.json())
-      //Then with the data from the response in JSON...
-      .then((response) => {
-        console.log('Success:', response);
-      })
-      .catch((response) => {
-        console.error('Error:', response);
-      });
-
-
-  }
-  bookSession(singSessionInfo: any) {
-    console.log("Inside service ::  Book Session")
-    console.log("JSON TO BE SENT", JSON.stringify(singSessionInfo))
-    fetch(apiendpoints.BOOK_SESSION, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(singSessionInfo),
-    })
-      .then((response) => response.json())
-      //Then with the data from the response in JSON...
-      .then((response) => {
-        console.log('Success:', response);
-      })
-      .catch((response) => {
-        console.error('Error:', response);
-      });
+    const headers = { 'Content-Type': 'application/json' };
+    const body = { "firstName": profile.value['firstName'] ,
+                    "lastName" : profile.value['lastName'],
+                    "email" : profile.value['email'],
+                    "password" : profile.value['password']
+                  };
+    console.log("Body ==>", body)
+    return  this.http.post(apiendpoints.REGISTER_MEMBER_URL, body, {headers}).pipe(catchError(this.handleError));
   }
 
-  cancelSession(singSessionInfo: any) {
-    console.log("Inside service ::  Book Session")
-    console.log("JSON TO BE SENT", JSON.stringify(singSessionInfo))
-    fetch(apiendpoints.BOOK_SESSION, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(singSessionInfo),
-    })
-      .then((response) => response.json())
-      //Then with the data from the response in JSON...
-      .then((response) => {
-        console.log('Success:', response);
-      })
-      .catch((response) => {
-        console.error('Error:', response);
-      });
+  registerNewTrainer(profile: FormGroup)
+  {
+    console.log("INSIDE SERVICE ::registerNewTrainer ")
+    const headers = { 'Content-Type': 'application/json'  };
+    const body = { firstName: profile.value['firstName'] ,
+                    lastName : profile.value['lastName'],
+                    email : profile.value['email'],
+                    password : profile.value['password']
+                  };
+    console.log("Body ==>", body)
+    return  this.http.post(apiendpoints.REGISTER_TRAINER_URL, body, {headers}).pipe(catchError(this.handleError));
+
   }
 
+  authenticateAndloadProfileData(loginForm: FormGroup) 
+  {
+      console.log("INSIDE SERVICE ::authenticateAndloadProfileData ")
+  
+      const headers = { 'Content-Type': 'application/json'  };
+      const body = { username: loginForm.value['email'] ,
+                     password : loginForm.value['password']
+                   };
+      console.log("Body ==>", body)
+      return  this.http.post(apiendpoints.AUTHENTICATE_AND_LOAD_URL, body, {headers}).pipe(catchError(this.handleError));
+  }
+
+  handleError(error: HttpErrorResponse) 
+  {
+      console.log("INSIDE SERVICE ::handleError ")
+      let errorMessage = 'Unknown error!';
+      if (error.error instanceof ErrorEvent) {
+        // Client-side errors
+        errorMessage = `Error: ${error.error.message}`;
+      } else {
+        // Server-side errors
+        errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+        if(error.status == 404)
+        {
+          errorMessage = "API Connection Failed :: 404";
+        }
+      }
+      window.alert(errorMessage);
+      return throwError(errorMessage);
+  }
+  
 }
 
 
+
+
+
+
+//   authenticateUser(loginForm: FormGroup) {
+//     return this.http.get<boolean>('');
+//   }
+  
+//   bookSession(singSessionInfo: any) {
+//     console.log("Inside service ::  Book Session")
+//     console.log("JSON TO BE SENT", JSON.stringify(singSessionInfo))
+//     fetch(apiendpoints.BOOK_SESSION, {
+//       method: 'POST',
+//       headers: { 'Content-Type': 'application/json' },
+//       body: JSON.stringify(singSessionInfo),
+//     })
+//       .then((response) => response.json())
+//       //Then with the data from the response in JSON...
+//       .then((response) => {
+//         console.log('Success:', response);
+//       })
+//       .catch((response) => {
+//         console.error('Error:', response);
+//       });
+//   }
+
+//   cancelSession(singSessionInfo: any) {
+//     console.log("Inside service ::  Book Session")
+//     console.log("JSON TO BE SENT", JSON.stringify(singSessionInfo))
+//     fetch(apiendpoints.BOOK_SESSION, {
+//       method: 'POST',
+//       headers: { 'Content-Type': 'application/json' },
+//       body: JSON.stringify(singSessionInfo),
+//     })
+//       .then((response) => response.json())
+//       //Then with the data from the response in JSON...
+//       .then((response) => {
+//         console.log('Success:', response);
+//       })
+//       .catch((response) => {
+//         console.error('Error:', response);
+//       });
+//   }
+
+// }
+
+// fetch(apiendpoints.REGISTER_MEMBER_URL, {
+//   method: 'POST',
+//   headers: { 'Content-Type': 'application/json'  },
+//   body: JSON.stringify(profile),
+// })
+//   .then((response) => response.json())
+  
+//   .then((response) => {
+//     console.log('Success:', response);
+//   })
+//   .catch((response) => {
+//     console.error('Error:', response);
+//   });
--- User-Interface/src/app/user-registration/user-registration.component.ts
+++ User-Interface/src/app/user-registration/user-registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ScheduleSessionService } from '../services/schedule-session.service';
 
 @Component({
@@ -9,27 +9,44 @@ import { ScheduleSessionService } from '../services/schedule-session.service';
 })
 export class UserRegistrationComponent implements OnInit {
   profileForm!: FormGroup;
-  constructor(private scheduleSessionService:ScheduleSessionService) { }
+  constructor(private scheduleSessionService: ScheduleSessionService) { }
   ngOnInit() {
-  
-  this.profileForm = new FormGroup
-  ({
 
-  firstName: new FormControl(''),
-  lastName: new FormControl(''),
-  email: new FormControl('', [Validators.email]),
-  pwd: new FormControl('',[Validators.required, Validators.minLength(6)]),
-  type: new FormControl('')
-    });
+    this.profileForm = new FormGroup
+      ({
+
+        firstName: new FormControl(''),
+        lastName: new FormControl(''),
+        email: new FormControl('', [Validators.email]),
+        password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+        type: new FormControl('')
+      });
   }
 
 
-  registerNewMember() {// TODO: Use EventEmitter with form value
-    if(this.profileForm.invalid)
-    {
+  registerNewUser(profileForm: FormGroup) {// TODO: Use EventEmitter with form value
+    if (profileForm.invalid) {
       return
     }
-    console.warn(this.profileForm.value);
-    this.scheduleSessionService.registerUser(this.profileForm.value);
+    console.warn(profileForm.value);
+    console.log(" type ==>", profileForm.controls['type'].value);
+
+    if (profileForm.controls['type'].value == 'member')
+    {
+      this.scheduleSessionService.registerNewMember(profileForm).subscribe((data: any)=>{
+        console.log(data);
+        // this.products = data;
+      });
+
+    }
+    else 
+    {
+
+      this.scheduleSessionService.registerNewTrainer(profileForm).subscribe((data: any)=>{
+        console.log(data);
+        // this.products = data;
+      });
+    }
+
   }
 }
