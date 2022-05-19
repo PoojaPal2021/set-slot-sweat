9284e07b2f2eaf71f7ee24bd384b0458cf9cffff
jardiamj <1088732+jardiamj@users.noreply.github.com>
2022-05-19 10:18:25 -0700

    Enable CORS on localhost 8080 for testing purposes

--- services/users/src/main/java/edu/uwb/gymapp/UserManagementApplication.java
+++ services/users/src/main/java/edu/uwb/gymapp/UserManagementApplication.java
@@ -3,7 +3,10 @@ package edu.uwb.gymapp;
 import edu.uwb.gymapp.models.MemberRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = {MemberRepository.class})
@@ -13,5 +16,13 @@ public class UserManagementApplication {
 		SpringApplication.run(UserManagementApplication.class, args);
 	}
 
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:8080");
+			}
+		};
+	}
 }
