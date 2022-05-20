0900db593159c3bb1e3f01dedb87b1c378d57ad2
jardiamj <1088732+jardiamj@users.noreply.github.com>
2022-05-19 18:10:40 -0700

    Removed commented out lines in Member class

--- services/users/src/main/java/edu/uwb/gymapp/models/Member.java
+++ services/users/src/main/java/edu/uwb/gymapp/models/Member.java
@@ -14,9 +14,6 @@ public class Member {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
-//    @Column(nullable = false)
-//    private String username;
-
     @NaturalId
     @Column(nullable = false, unique = true)
     private String email;
@@ -54,15 +51,7 @@ public class Member {
     public void setId(Long id) {
         this.id = id;
     }
-
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-
+    
     public String getEmail() {
         return email;
     }
