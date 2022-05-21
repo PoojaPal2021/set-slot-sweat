c676e808475887a5ea5dc5d08109ea216cba5ded
jardiamj <1088732+jardiamj@users.noreply.github.com>
2022-05-21 13:59:41 -0700

    Added logging facilities and doc-comments

--- services/reservations/src/main/java/edu/uwb/gymapp/resources/ReservationController.java
+++ services/reservations/src/main/java/edu/uwb/gymapp/resources/ReservationController.java
@@ -1,9 +1,10 @@
 package edu.uwb.gymapp.resources;
 
-import edu.uwb.gymapp.models.Member;
 import edu.uwb.gymapp.models.Reservation;
 import edu.uwb.gymapp.models.Session;
 import edu.uwb.gymapp.workoutsession.SessionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.*;
@@ -21,6 +22,8 @@ import java.util.NoSuchElementException;
 @RequestMapping("/reservation-service/api/v1")
 public class ReservationController {
 
+    Logger logger = LoggerFactory.getLogger(ReservationController.class);
+
     @Autowired
     public ReservationService reservationService;
 
@@ -37,6 +40,11 @@ public class ReservationController {
         return "Welcome: " + principal.getName();
     }
 
+    /**
+     * Authenticates member credentials and sets an authentication token for future requests
+     * @param memberDetails The member details with its username (email) and password
+     * @return  The list of reservations for the given member
+     */
     @RequestMapping(value="/member/login", method = RequestMethod.POST)
     public List<Reservation> login(@RequestBody MemberDetails memberDetails) {
 
@@ -46,24 +54,38 @@ public class ReservationController {
         try {
             authentication = authenticationManager.authenticate(authReq);
         } catch (Exception e) {
+            logger.debug("Unable to authenticate user with email: " + memberDetails.getUsername());
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unable to authenticate user. Try again.");
         }
 
         List<Reservation> reservations = reservationService.getAllReservations(memberDetails.getUsername());
 
+        logger.info("Successful login for Member: " + memberDetails.getUsername());
         return reservations;
     }
 
+    /**
+     * Log member out of the service API
+     */
     @RequestMapping(value="/member/logout", method = RequestMethod.POST)
     public void logout() {
+        String username = authentication.getName();
         authentication = null;
+        logger.info("Successful logout for Member: " + username);
     }
 
+    /**
+     * Books the session with the given id for the member with the given email
+     * @param email The email of the member booking the session
+     * @param sessionId The id of the session to be booked
+     * @return  The list of sessions currently booked and not booked
+     */
     @RequestMapping(value="/session/book/{sessionId}", params = "email", method = RequestMethod.POST)
     public List<Reservation> addReservation(@RequestParam("email") String email,
                                @PathVariable Long sessionId) {
         // Authenticate
         if (authentication == null || !authentication.getName().equals(email)) {
+            logger.debug("Blocked booking access to user: " + email);
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Access not allowed for: " + email);
         }
 
@@ -72,6 +94,7 @@ public class ReservationController {
         try {
             session = sessionService.getSession(sessionId);
         } catch (NoSuchElementException ex) {
+            logger.debug("Session ID couldn't be found: " + sessionId);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Session ID couldn't be found: " + sessionId);
         }
 
@@ -83,46 +106,71 @@ public class ReservationController {
         try {
             status = reservationService.addReservation(reservation);
         } catch (RuntimeException ex) {
+            logger.debug("Failed to book session with id: " + sessionId);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
                     "Reservation failed: " + ex.getCause().getCause().getMessage());
         }
 
         if (!status.equals("SUCCESS")) {
+            logger.debug(status + ": Failed to book session with id " + sessionId);
             throw new ResponseStatusException(HttpStatus.CONFLICT, status);
         }
         List<Reservation> reservations = reservationService.getAllReservations(email);
+
+        logger.info("Successfully booked Session with ID: " + sessionId);
         return reservations;
     }
 
+    /**
+     * Cancel the reservation with the given id and member email address
+     * @param email The email address of the member cancelling the reservation
+     * @param reservationId The id of the reservation to be cancelled
+     * @return  Success or failure message string
+     */
     @RequestMapping(value="/session/cancel/{reservationId}", method=RequestMethod.DELETE)
     public String cancelReservation(@RequestParam("email") String email,
                                     @PathVariable Long reservationId) {
         // Authenticate
         if (authentication == null || !authentication.getName().equals(email)) {
+            logger.debug("Blocked reservation cancelling access to user: " + email);
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Access not allowed for: " + email);
         }
 
         try {
             reservationService.deleteReservation(reservationId);
         } catch (EmptyResultDataAccessException ex) {
+            logger.debug("No reservation exists for id: " + reservationId);
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No reservation exists for id: " + reservationId);
         }
 
+        logger.info("Successfully cancelled reservation with id: " + reservationId);
         return "Successfully cancelled reservation.";
     }
 
+    /**
+     * Retrieve the list of reservations for the given member email
+     * @param email The email address of the member retrieving the list of reservations
+     * @return The list of reservations for the given member
+     */
     @RequestMapping(value="/reservations", params="email", method = RequestMethod.GET)
     public List<Reservation> getAllReservations(@RequestParam("email") String email) {
         // Verify that we are accessing the reservations for the current user
         if (authentication == null || !authentication.getName().equals(email)) {
+            logger.debug("Blocked reservations access to user: " + email);
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Access not allowed for: " + email);
         }
 
+        logger.info("All reservations have been retrieved for user: " + email);
         return reservationService.getAllReservations(email);
     }
 
+    /**
+     * Retrieve the list of reservations
+     * @return The list of reservations
+     */
     @RequestMapping("/reservations")
     public List<Reservation> getAllReservations() {
+        logger.info("All reservations have been retrieved by user: " + authentication.getName());
         return reservationService.getAllReservations();
     }
 }
--- services/reservations/src/main/java/edu/uwb/gymapp/resources/TrainerController.java
+++ services/reservations/src/main/java/edu/uwb/gymapp/resources/TrainerController.java
@@ -1,10 +1,11 @@
 package edu.uwb.gymapp.resources;
 
-import edu.uwb.gymapp.models.Member;
 import edu.uwb.gymapp.models.Trainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -13,37 +14,70 @@ import org.springframework.web.server.ResponseStatusException;
 @RequestMapping("/reservation-service/api/v1")
 public class TrainerController {
 
+    Logger logger = LoggerFactory.getLogger(TrainerController.class);
+
     @Autowired
     private TrainerService trainerService;
 
+    /**
+     * Retrieve information about the trainer with the given email address
+     * @param email The trainer email address
+     * @return  The information about the trainer with the given email address
+     */
     @RequestMapping(value="/trainers", params="email", method= RequestMethod.GET)
     public Trainer getTrainer(@RequestParam("email") String email) {
+        logger.info("Retrieved information for trainer: " + email);
         return trainerService.getTrainer(email);
     }
 
+    /**
+     * Retrieve information about the trainer with the given id
+     * @param id    The trainer id
+     * @return  The information about the trainer with the given id
+     */
     @RequestMapping(value="/trainer/{id}")
     public Trainer getTrainer(@PathVariable Long id) {
+        logger.info("Retrieved information for trainer with id: " + id);
         return trainerService.getTrainer(id);
     }
 
+    /**
+     * Add a new trainer to the database. The trainer information must be given in the request body in JSON format
+     * @param trainer   JSON object with all trainer information
+     * @return  Success or failure message string
+     */
     @RequestMapping(method= RequestMethod.POST, value="/trainer/signup")
     public Trainer addTrainer(@RequestBody Trainer trainer) {
         try {
             Trainer newTrainer = trainerService.addTrainer(trainer);
+            logger.info("Successfully created profile for trainer with email: " + trainer.getEmail());
             return newTrainer;
+        } catch (DataIntegrityViolationException ex) {
+            logger.debug("Sign up failed. Email already exists in the system: " + trainer.getEmail());
+            throw  new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Sign Up Failed. Email already exists in the system: " + trainer.getEmail());
         } catch (RuntimeException ex) {
+            logger.debug("Failed to create trainer profile.");
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
                     "Failed to create user profile. Review your information and try again.");
         }
     }
 
+    /**
+     * Update information for the given trainer
+     * @param trainer   JSON object with all trainer information
+     */
     @RequestMapping(method=RequestMethod.PUT, value="/trainer/{id}")
-    public void updateMember(@RequestBody Trainer trainer) {
+    public void updateTrainer(@RequestBody Trainer trainer) {
         trainerService.updateTrainer(trainer);
     }
 
+    /**
+     * Delete trainer with the given id
+     * @param id    The id of the trainer to be deleted
+     */
     @RequestMapping(method=RequestMethod.DELETE, value="/trainer/{id}")
-    public void deleteMember(@PathVariable Long id) {
+    public void deleteTrainer(@PathVariable Long id) {
         trainerService.deleteTrainer(id);
     }
 }
